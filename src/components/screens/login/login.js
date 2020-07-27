a69b1ac31832b266740743effcf820ee7500f73e
mohammed <slimani.med.21@gmail.com>
2020-07-27 12:39:57 +0200

    :construction_worker: Added facebook Ouath

--- src/components/screens/login/login.js
+++ src/components/screens/login/login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, StyleSheet, Button } from 'react-native'
+import {Entypo} from "@expo/vector-icons"
+import { facebook } from "../../../constants/facebook"
+import * as Facebook from 'expo-facebook';
 class Login extends Component {
     constructor(props) {
         super(props)
@@ -12,13 +14,50 @@ class Login extends Component {
 
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>
-                    This is the login screen
+                    Welcome to Foodinator
                 </Text>
+                <Button title="Login with Facebook" onPress={logIn}>
+                    <Entypo name="facebook" size= {32} color="#3b5998" />
+                </Button>
             </View>
         )
     }
 }
+
+async function logIn() {
+    try {
+      await Facebook.initializeAsync(facebook.id);
+      const {
+        type,
+        token,
+        expires,
+        permissions,
+        declinedPermissions,
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ['public_profile'],
+      });
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+        Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  }
+  
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 
 export default Login
